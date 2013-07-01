c3cb760e36e47ad6b9fbddc18f269cbd164d50cf
Brian Hawkins <brianhks1@gmail.com>
2013-07-01 13:10:54 -0600

    started updates to json response

--- src/main/java/org/kairosdb/core/formatter/JsonResponse.java
+++ src/main/java/org/kairosdb/core/formatter/JsonResponse.java
@@ -1,6 +1,11 @@
 package org.kairosdb.core.formatter;
 
+import org.json.JSONException;
+import org.json.JSONWriter;
+import org.kairosdb.core.DataPoint;
+import org.kairosdb.core.datastore.DataPointGroup;
 import org.kairosdb.core.datastore.QueryResults;
+import org.kairosdb.core.groupby.GroupByResult;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -15,30 +20,133 @@ import java.io.Writer;
 public class JsonResponse
 {
 	private Writer m_writer;
+	private JSONWriter m_jsonWriter;
+	private boolean m_began = false;
+	private boolean m_beganQueries = false;
 
 	public JsonResponse(Writer writer)
 	{
 		m_writer = writer;
+		m_jsonWriter = new JSONWriter(writer);
 	}
 
 
 	void begin() throws FormatterException
 	{
+		try
+		{
+			m_jsonWriter.object();
+			m_began = true;
+		}
+		catch (JSONException e)
+		{
+			throw new FormatterException(e);
+		}
 	}
 
 
 	void startQueries() throws FormatterException
 	{
+		try
+		{
+			m_jsonWriter.key("queries").array();
+			m_beganQueries = true;
+		}
+		catch (JSONException e)
+		{
+			throw new FormatterException(e);
+		}
+
 	}
 
 
 	void formatQuery(QueryResults queryResults) throws FormatterException
 	{
+		try
+		{
+			m_jsonWriter.object().key("results").array();
+
+			for (DataPointGroup group : queryResults.getDataPoints())
+			{
+				final String metric = group.getName();
+
+				m_jsonWriter.object();
+				m_jsonWriter.key("name").value(metric);
+
+				if (!group.getGroupByResult().isEmpty())
+				{
+					m_jsonWriter.key("group_by");
+					m_jsonWriter.array();
+					boolean first = true;
+					for (GroupByResult groupByResult : group.getGroupByResult())
+					{
+						if (!first)
+							m_writer.write(",");
+						m_writer.write(groupByResult.toJson());
+						first = false;
+					}
+					m_jsonWriter.endArray();
+				}
+
+				m_jsonWriter.key("tags").object();
+
+				for (String tagName : group.getTagNames())
+				{
+					m_jsonWriter.key(tagName);
+					m_jsonWriter.value(group.getTagValues(tagName));
+				}
+				m_jsonWriter.endObject();
+
+				m_jsonWriter.key("values").array();
+				while (group.hasNext())
+				{
+					DataPoint dataPoint = group.next();
+
+					m_jsonWriter.array().value(dataPoint.getTimestamp());
+					if (dataPoint.isInteger())
+					{
+						m_jsonWriter.value(dataPoint.getLongValue());
+					}
+					else
+					{
+						final double value = dataPoint.getDoubleValue();
+						if (value != value || Double.isInfinite(value))
+						{
+							throw new IllegalStateException("NaN or Infinity:" + value + " data point=" + dataPoint);
+						}
+						m_jsonWriter.value(value);
+					}
+					m_jsonWriter.endArray();
+				}
+				m_jsonWriter.endArray();
+				m_jsonWriter.endObject();
+
+				group.close();
+			}
+		}
+		catch (JSONException e)
+		{
+			throw new FormatterException(e);
+		}
+		catch (IOException e)
+		{
+			throw new FormatterException(e);
+		}
 	}
 
 
 	void endQueries() throws FormatterException
 	{
+		try
+		{
+			m_jsonWriter.endArray();
+			m_beganQueries = false;
+		}
+		catch (JSONException e)
+		{
+			throw new FormatterException(e);
+		}
+
 	}
 
 
@@ -49,7 +157,14 @@ public class JsonResponse
 
 	void end() throws FormatterException
 	{
-
+		try
+		{
+			m_jsonWriter.endObject();
+		}
+		catch (JSONException e)
+		{
+			throw new FormatterException(e);
+		}
 	}
 
 }

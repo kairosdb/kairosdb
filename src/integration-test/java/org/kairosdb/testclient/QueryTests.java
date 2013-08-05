38a5b8192bc25728fbc76c9f1013656b20e7baec
Jeff Sabin <jsabin@proofpoint.com>
2013-08-05 16:52:31 -0600

    Test names are now dynamically created. Added the ability to add data points. Delete all added datapoints when the test is complete. If the resulting value is a double, use the closeTo assertions.

--- src/integration-test/java/org/kairosdb/testclient/QueryTests.java
+++ src/integration-test/java/org/kairosdb/testclient/QueryTests.java
@@ -16,7 +16,9 @@
 
 package org.kairosdb.testclient;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
@@ -29,11 +31,19 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static com.google.common.base.Preconditions.checkState;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
 
 
 /*
@@ -42,6 +52,8 @@ List of tests we need to perform
 2. test that returns data
 3. test that will use the cache file to return data.
 4. Nice to verify no open file handles after tests have ran.
+5. Test aggregators (multiple)
+6. Test group by
  */
 
 public class QueryTests
@@ -51,26 +63,30 @@ public class QueryTests
 	private String m_port = "8080";
 
 	@Parameters({"host", "port"})
-	public QueryTests(String m_host, String m_port)
+	public QueryTests(String host, String port)
 	{
-		this.m_host = m_host;
-		this.m_port = m_port;
+		m_host = host;
+		m_port = port;
 	}
 
-	private JsonElement readJsonFromStream(String path) throws IOException, JSONException
+	private JsonElement readJsonFromStream(String path, String metricName) throws IOException, JSONException
 	{
 		InputStream is = ClassLoader.getSystemResourceAsStream(path);
 		if (is == null)
 			return (null);
 
 		String str = IOUtils.toString(is);
+
+		//replace metric name
+		str = str.replace("<metric_name>", metricName);
+
 		return (m_parser.parse(str));
 	}
 
 	private JsonElement postQuery(JsonElement query) throws IOException, JSONException
 	{
 		HttpClient client = new DefaultHttpClient();
-		HttpPost post = new HttpPost("http://"+m_host+":" + m_port + "/api/v1/datapoints/query");
+		HttpPost post = new HttpPost("http://" + m_host + ":" + m_port + "/api/v1/datapoints/query");
 		post.setHeader("Content-Type", "application/json");
 
 		post.setEntity(new StringEntity(query.toString()));
@@ -85,34 +101,187 @@ public class QueryTests
 		return (m_parser.parse(IOUtils.toString(httpResponse.getEntity().getContent())));
 	}
 
+	private int putDataPoints(JsonElement dataPoints) throws IOException, JSONException
+	{
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost("http://" + m_host + ":" + m_port + "/api/v1/datapoints");
+		post.setHeader("Content-Type", "application/json");
+
+		post.setEntity(new StringEntity(dataPoints.toString()));
+		HttpResponse httpResponse = client.execute(post);
+
+		return httpResponse.getStatusLine().getStatusCode();
+	}
+
+	private int deleteDataPoints(JsonElement query) throws IOException, JSONException
+	{
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost("http://" + m_host + ":" + m_port + "/api/v1/datapoints/delete");
+		post.setHeader("Content-Type", "application/json");
+
+		post.setEntity(new StringEntity(query.toString()));
+		HttpResponse httpResponse = client.execute(post);
+
+		return httpResponse.getStatusLine().getStatusCode();
+	}
 
 	@DataProvider(name = "query-provider")
-	public Object[][] getQueryTests() throws IOException, JSONException
+	public Object[][] getQueryTests() throws IOException, JSONException, URISyntaxException
 	{
 		ArrayList<Object[]> ret = new ArrayList<Object[]>();
 
-		ClassLoader cl = this.getClass().getClassLoader();
+		List<String> resourceDirectoryNames = getTestDirectories("tests");
 
-		for (int test = 1;; test++)
+		for (String resourceDirectory : resourceDirectoryNames)
 		{
-			JsonElement query = readJsonFromStream("tests/test_" + test + "/query.json");
-			JsonElement response = readJsonFromStream("tests/test_"+ test + "/response.json");
+			String metricName = "integration_test_" + UUID.randomUUID();
+			JsonElement dataPoints = readJsonFromStream("tests/" + resourceDirectory + "/datapoints.json", metricName);
+			JsonElement query = readJsonFromStream("tests/" + resourceDirectory + "/query.json", metricName);
+			JsonElement response = readJsonFromStream("tests/" + resourceDirectory + "/response.json", metricName);
 
-			if (query == null)
-				break;
+			checkState(query != null, "No query found for test " + resourceDirectory);
 
-			ret.add(new Object[]{query, response});
+			ret.add(new Object[]{resourceDirectory, dataPoints, query, response});
 		}
 
 		return (ret.toArray(new Object[0][]));
 	}
 
-
 	@Test(dataProvider = "query-provider")
-	public void performQueryTest(JsonElement query, JsonElement response) throws IOException, JSONException
+	public void performQueryTest(String testName, JsonElement dataPoints, JsonElement query, JsonElement response)
+			throws IOException, JSONException, InterruptedException
 	{
-		JsonElement serverResponse = postQuery(query);
+		int retryCount = 0;
+		if (dataPoints != null)
+		{
+			int status = putDataPoints(dataPoints);
+			assertThat(status, equalTo(204));
+			retryCount = 3;
+		}
 
-		assertEquals(response, serverResponse);
+		do
+		{
+			try
+			{
+				JsonElement serverResponse = postQuery(query);
+				assertResponse(testName, serverResponse, response);
+				break;
+			}
+			catch (AssertionError e)
+			{
+				if (retryCount == 0)
+					throw e;
+
+				retryCount--;
+				Thread.sleep(500); // Need to wait until datapoints are available in the data store
+			}
+		} while (true);
+
+		if (dataPoints != null)
+		{
+			// clean up
+			int status = deleteDataPoints(query);
+
+			assertThat(status, equalTo(204));
+
+			// todo verify that data points are gone
+		}
+	}
+
+	private void assertResponse(String testName, JsonElement actual, JsonElement expected)
+	{
+		JsonArray actualQueries = actual.getAsJsonObject().get("queries").getAsJsonArray();
+		JsonArray expectedQueries = expected.getAsJsonObject().get("queries").getAsJsonArray();
+
+		assertThat("Number of queries is different for test: " + testName, actualQueries.size(), equalTo(expectedQueries.size()));
+
+		for (int i = 0; i < expectedQueries.size(); i++)
+		{
+			JsonArray actualResult = actualQueries.get(i).getAsJsonObject().get("results").getAsJsonArray();
+			JsonArray expectedResult = expectedQueries.get(i).getAsJsonObject().get("results").getAsJsonArray();
+
+			assertThat("Number of results is different for test: " + testName, actualResult.size(), equalTo(expectedResult.size()));
+
+			for (int j = 0; j < expectedResult.size(); j++)
+			{
+				JsonObject actualMetric = actualResult.get(j).getAsJsonObject();
+				JsonObject expectedMetric = expectedResult.get(j).getAsJsonObject();
+
+				assertThat("Metric name is different for test: " + testName, actualMetric.get("name"), equalTo(expectedMetric.get("name")));
+				assertTags(testName, actualMetric, expectedMetric);
+				assertDataPoints(testName, i, j, actualMetric, expectedMetric);
+			}
+		}
+	}
+
+	private void assertTags(String testName, JsonObject actual, JsonObject expected)
+	{
+		JsonObject actualTags = actual.getAsJsonObject("tags");
+		JsonObject expectedTags = expected.getAsJsonObject("tags");
+
+		for (Map.Entry<String, JsonElement> tag : expectedTags.entrySet())
+		{
+			String tagName = tag.getKey();
+			assertThat("Missing tag: " + tagName + " for test " + testName, actualTags.has(tagName), equalTo(true));
+			assertThat("Tag value different for key: " + tagName + " for test:" + testName, actualTags.get(tagName), equalTo(tag.getValue()));
+		}
+	}
+
+	private void assertDataPoints(String testName, int queryCount, int resultCount, JsonObject actual, JsonObject expected)
+	{
+		JsonArray actualValues = actual.getAsJsonArray("values");
+		JsonArray expectedValues = expected.getAsJsonArray("values");
+
+		assertThat(String.format("Number of datapoints is different for test %s, query: %d, result: %d",
+				testName, queryCount, resultCount),
+				actualValues.size(), equalTo(expectedValues.size()));
+
+		for (int i = 0; i < expectedValues.size(); i++)
+		{
+			assertThat(String.format("Timestamps different for data point %d for test %s, query: %d, result: %d",
+					i, testName, queryCount, resultCount),
+					actualValues.get(i).getAsJsonArray().get(0), equalTo(expectedValues.get(i).getAsJsonArray().get(0)));
+
+
+			if (isDouble(actualValues.get(i).getAsJsonArray().get(1)))
+				assertThat(String.format("Values different for data point: %d for test %s, query: %d, result: %d",
+						i, testName, queryCount, resultCount),
+						actualValues.get(i).getAsJsonArray().get(1).getAsDouble(),
+						closeTo(expectedValues.get(i).getAsJsonArray().get(1).getAsDouble(), .01));
+			else
+				assertThat(String.format("Values different for data point: %d for test: %s, query: %d, result: %d",
+						i, testName, queryCount, resultCount),
+						actualValues.get(i).getAsJsonArray().get(1), equalTo(expectedValues.get(i).getAsJsonArray().get(1)));
+		}
+	}
+
+	private boolean isDouble(JsonElement value)
+	{
+		return value.toString().contains(".");
+	}
+
+	public static List<String> getTestDirectories(String matchingDirectoryName) throws URISyntaxException, IOException
+	{
+		return findTestDirectories(new File("src/integration-test/resources"), matchingDirectoryName);
+	}
+
+	@SuppressWarnings("ConstantConditions")
+	private static List<String> findTestDirectories(File directory, String matchingDirectoryName)
+	{
+		List<String> matchingDirectories = new ArrayList<String>();
+
+		for (File file : directory.listFiles())
+		{
+			if (file.isDirectory())
+			{
+				if (file.getParentFile().getName().equals(matchingDirectoryName))
+				{
+					matchingDirectories.add(file.getName());
+				}
+				else
+					matchingDirectories.addAll(findTestDirectories(file, matchingDirectoryName));
+			}
+		}
+		return matchingDirectories;
 	}
 }

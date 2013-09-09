d5eca293751dc56ff79ff1b89e6f8fcffd297ec5
Brian Hawkins <brianhks1@gmail.com>
2013-09-09 08:45:04 -0600

    Updated anomaly detector

--- src/main/java/org/kairosdb/anomalyDetection/MedianStreamAnomalyDetector.java
+++ src/main/java/org/kairosdb/anomalyDetection/MedianStreamAnomalyDetector.java
@@ -19,6 +19,9 @@ package org.kairosdb.anomalyDetection;
 
 import org.kairosdb.core.DataPoint;
 
+import java.util.Date;
+import java.util.LinkedList;
+
 /**
  Created with IntelliJ IDEA.
  User: bhawkins
@@ -28,14 +31,17 @@ import org.kairosdb.core.DataPoint;
  */
 public class MedianStreamAnomalyDetector implements AnomalyAlgorithm
 {
-	private static final int TREE_SIZE = 100;
+	private static final int TREE_SIZE = 150;
+	private static final int LAST_VALUE_SIZE = 2;
 	private static final double FACTOR = 2.5;
 
 	private AnalyzerTreeMap<AnalyzerDataPoint, String> m_tree;
+	private LinkedList<Double> m_lastValues;
 
 	public MedianStreamAnomalyDetector()
 	{
 		m_tree = new AnalyzerTreeMap<AnalyzerDataPoint, String>(TREE_SIZE);
+		m_lastValues = new LinkedList<Double>();
 	}
 
 	@Override
@@ -44,7 +50,7 @@ public class MedianStreamAnomalyDetector implements AnomalyAlgorithm
 		if (!metricName.equals("my_test"))
 			return false;
 
-		boolean anomaly = false;
+		/*boolean anomaly = false;
 
 		if (m_tree.size() == TREE_SIZE)
 		{
@@ -62,8 +68,27 @@ public class MedianStreamAnomalyDetector implements AnomalyAlgorithm
 		}
 
 		if (!anomaly)
-			m_tree.put(new AnalyzerDataPoint(dataPoint.getTimestamp(), dataPoint.getDoubleValue()), null);
+			m_tree.put(new AnalyzerDataPoint(dataPoint.getTimestamp(), dataPoint.getDoubleValue()), null);*/
 
-		return anomaly;
+		double ret = 0.0;
+
+		m_tree.put(new AnalyzerDataPoint(dataPoint.getTimestamp(), dataPoint.getDoubleValue()), null);
+
+		double newValue = m_tree.lastKey().getValue() - m_tree.firstKey().getValue();
+		m_lastValues.addFirst(newValue);
+
+		if (m_tree.size() == TREE_SIZE)
+		{
+			double lastValue = m_lastValues.getLast();
+			if ((newValue > lastValue) && ((newValue - lastValue) > (lastValue * 0.33)))
+			{
+				System.out.println("Anomaly found at "+ new Date(dataPoint.getTimestamp()) + " " + ((newValue - lastValue) / lastValue));
+			}
+		}
+
+		if (m_lastValues.size() > LAST_VALUE_SIZE)
+			m_lastValues.removeLast();
+
+		return false;
 	}
 }

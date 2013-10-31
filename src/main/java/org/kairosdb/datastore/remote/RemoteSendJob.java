5fce941a5b85da7cc9dc00a8ae3cc400d253ec4c
Jeff Sabin <jsabin@proofpoint.com>
2013-10-31 09:38:53 -0600

    Updated copyright.

--- src/main/java/org/kairosdb/datastore/remote/RemoteSendJob.java
+++ src/main/java/org/kairosdb/datastore/remote/RemoteSendJob.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Proofpoint Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package org.kairosdb.datastore.remote;
 
 import com.google.inject.Inject;
@@ -15,13 +31,6 @@ import java.util.Random;
 
 import static org.quartz.TriggerBuilder.newTrigger;
 
-/**
- Created with IntelliJ IDEA.
- User: bhawkins
- Date: 6/3/13
- Time: 4:22 PM
- To change this template use File | Settings | File Templates.
- */
 public class RemoteSendJob implements KairosDBJob
 {
 	public static final Logger logger = LoggerFactory.getLogger(RemoteSendJob.class);

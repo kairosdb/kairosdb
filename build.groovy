3454bbc37351b5b4b7a0ab7748928c1daac4966a
Brian Hawkins <brianhks1@gmail.com>
2014-07-31 20:51:49 -0600

    updated build number for legacy fix

--- build.groovy
+++ build.groovy
@@ -28,7 +28,7 @@ saw.setProperty(Tablesaw.PROP_MULTI_THREAD_OUTPUT, Tablesaw.PROP_VALUE_ON)
 programName = "kairosdb"
 //Do not use '-' in version string, it breaks rpm uninstall.
 version = "0.9.4"
-release = "5" //package release number
+release = "6" //package release number
 summary = "KairosDB"
 description = """\
 KairosDB is a time series database that stores numeric values along

1d7a6544dec4fe1add4c779564f6e77ab87fdb1c
Brian Hawkins <brianhks1@gmail.com>
2015-04-03 07:24:50 -0600

    Release 0.9.5 beta 2

--- build.groovy
+++ build.groovy
@@ -27,7 +27,7 @@ saw.setProperty(Tablesaw.PROP_MULTI_THREAD_OUTPUT, Tablesaw.PROP_VALUE_ON)
 
 programName = "kairosdb"
 //Do not use '-' in version string, it breaks rpm uninstall.
-version = "0.9.5-SNAPSHOT"
+version = "0.9.5"
 release = "0.2beta" //package release number
 summary = "KairosDB"
 description = """\

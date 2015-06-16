ff60d33d599132f850091eede453bcdc5c811856
Jeff Sabin <jsabin@proofpoint.com>
2015-06-16 15:23:47 -0600

    Change rollup test from 30 minutes to 10 minutes.

--- src/test/scripts/addRollups.sh
+++ src/test/scripts/addRollups.sh
@@ -4,7 +4,7 @@ curl -s http://localhost:8080/api/v1/rollups/rollup -XPOST -H'Content-type:appli
 	[
 	    {
 	        "metric_name": "test_metric",
-	        "schedule": "*/30 * * * * ?",
+	        "schedule": "*/10 * * * * ?",
 	        "start_relative": {
 	            "value": 5,
 	            "unit": "minutes"

a593ff34cb95290fdd9b7e882fa1744d1e905698
Jeff Sabin <jsabin@proofpoint.com>
2015-06-17 06:52:35 -0600

    Updated job to get data from the context.

--- src/main/java/org/kairosdb/rollup/RollUpJob.java
+++ src/main/java/org/kairosdb/rollup/RollUpJob.java
@@ -1,17 +1,22 @@
 package org.kairosdb.rollup;
 
+import com.google.inject.Inject;
+import org.kairosdb.core.datastore.KairosDatastore;
+import org.kairosdb.core.datastore.QueryMetric;
+import org.kairosdb.core.http.rest.json.Metric;
 import org.kairosdb.core.scheduler.KairosDBSchedulerImpl;
 import org.quartz.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.inject.internal.util.$Preconditions.checkNotNull;
+import static com.google.inject.internal.util.$Preconditions.checkState;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 public class RollUpJob implements InterruptableJob
 {
 	private static final Logger log = LoggerFactory.getLogger(KairosDBSchedulerImpl.class);
-	private RollUpTask task;
+//	private final KairosDatastore datastore;
 
 	private boolean interrupted;
 
@@ -19,25 +24,21 @@ public class RollUpJob implements InterruptableJob
 	{
 	}
 
-	public RollUpJob(RollUpTask task)
-	{
-		this.task = checkNotNull(task);
-	}
-
-//	@Override
-	public Trigger getTrigger()
-	{
-		return newTrigger()
-				.withIdentity(task.getId() + "-" + this.getClass().getSimpleName())
-				.withSchedule(CronScheduleBuilder.cronSchedule(task.getSchedule()))
-				.build();
-	}
+	//	public RollUpJob(KairosDatastore datastore)
+//	{
+//		this.datastore = checkNotNull(datastore);
+//	}
 
 	@Override
 	public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException
 	{
 		JobDataMap dataMap = jobExecutionContext.getMergedJobDataMap();
 		RollUpTask task = (RollUpTask) dataMap.get("task");
+		checkState(task != null, "Task was null");
+
+		long now = System.currentTimeMillis();
+//		QueryMetric query = new QueryMetric(task.getStartTime().getTimeRelativeTo(now), task.getEndTime().getTimeRelativeTo(now));
+//		datastore.createQuery(task.)
 
 		log.info("Executing job " + task.getMetricName());
 

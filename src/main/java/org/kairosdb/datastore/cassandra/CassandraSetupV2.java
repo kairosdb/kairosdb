d8666724ef7f3eeba25144885498e4140fa8eae1
Jan M <janm81@gmail.com>
2016-10-19 16:57:46 +0200

    Fix v2 schema lookup

--- src/main/java/org/kairosdb/datastore/cassandra/CassandraSetupV2.java
+++ src/main/java/org/kairosdb/datastore/cassandra/CassandraSetupV2.java
@@ -16,7 +16,7 @@ public class CassandraSetupV2 extends  CassandraSetup {
 
     @Override
     protected boolean tableExists(Session session, String tableName) {
-        PreparedStatement ps = session.prepare("SELECT * FROM system.tables WHERE keyspace_name = ? and table_name = ?");
+        PreparedStatement ps = session.prepare("SELECT * FROM system.schema_columnfamilies WHERE keyspace_name = ? and columnfamily_name = ?");
         List<Row> rows = session.execute(ps.bind(keySpace, tableName)).all();
         return rows.size() == 1;
     }

488e7f72d747a3b879c6024a35ee770cd5403279
Brian Hawkins <brianhks1@gmail.com>
2017-01-04 09:50:37 -0700

    Fixed docs plugin list, updated version to 1.1.4

--- build.groovy
+++ build.groovy
@@ -28,7 +28,7 @@ saw.setProperty(Tablesaw.PROP_MULTI_THREAD_OUTPUT, Tablesaw.PROP_VALUE_ON)
 
 programName = "kairosdb"
 //Do not use '-' in version string, it breaks rpm uninstall.
-version = "1.1.3"
+version = "1.1.4"
 release = saw.getProperty("KAIROS_RELEASE_NUMBER", "1") //package release number
 summary = "KairosDB"
 description = """\

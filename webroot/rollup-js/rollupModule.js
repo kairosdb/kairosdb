d2bce3c52d20c86b9574bfff98b104149f08b8e5
jsabin <jsabin@proofpoint.com>
2018-01-01 14:35:56 -0700

    Fixed bug where populating the edit for sampling was overriding the existing values.

--- webroot/rollup-js/rollupModule.js
+++ webroot/rollup-js/rollupModule.js
@@ -548,32 +548,31 @@ module.directive('aggregatorproperties', function ($compile)
                 var name = parentName ? parentName + "." + property.name: property.name;
                 var html = "";
                 var modelName = 'agg.' + name;
-                if (property.type == 'boolean') {
+                if (property.type === 'boolean') {
                     html += "<span>" + property.name + "</span> ";
-                    html += "<input name='" + modelName + "' type='checkbox' ng-init=\"" + modelName + "=" + property.defaultValue + "\" + ng-model='" + modelName + "' >";
+                    html += "<input name='" + modelName + "' type='checkbox' ng-model='" + modelName + "' >";
                 }
-                else if (property.type == 'double') {
+                else if (property.type === 'double') {
                     html += "<span>" + property.name + "</span> ";
-                    html += "<input  class='small-input' type='text' ng-init=\"" + modelName + "=" + property.defaultValue + "\" + ng-model='" + modelName + "' style='width:30px'>";
+                    html += "<input  class='small-input' type='text' ng-model='" + modelName + "' style='width:30px'>";
                 }
-                else if (property.type == 'int' || property.type == 'long') {
+                else if (property.type === 'int' || property.type === 'long') {
                     html += "<span>" + property.name + "</span> ";
-                    html += "<input name='" + modelName + "' class='small-input' type='text' ng-init=\"" + modelName + "='" + property.defaultValue + "'\" + ng-model='" + modelName + "' value='" + property.defaultValue + "' style='width:30px'>";
+                    html += "<input name='" + modelName + "' class='small-input' type='text' ng-model='" + modelName + "' value='" + property.defaultValue + "' style='width:30px'>";
                 }
-                else if (property.type == 'String') {
+                else if (property.type === 'String') {
                     html += "<span>" + property.name + "</span> ";
-                    html += "<input class='small-input' type='text' ng-init=\"" + modelName + "='" + property.defaultValue + "'\" ng-model='" + modelName + "' style='width:90px' ng-model='agg." + property.name + "'>";
+                    html += "<input class='small-input' type='text' ng-model='" + modelName + "' style='width:90px' ng-model='agg." + property.name + "'>";
                 }
-                else if (property.type == "Object")
+                else if (property.type === "Object")
                 {
                     _.each(property.properties, function (subProperty)
                     {
                         html += scope.getHtml(subProperty, property.name);
                     });
                 }
-                else if (property.type == 'enum') {
+                else if (property.type === 'enum') {
                     scope.values = property.options;
-                    scope.setValue(scope.agg, modelName, property.defaultValue);
                     html += "<span>" + property.name + "</span> ";
                     html += '<div class="dropdown" style="display:inline-block">';
                     html += '	<button class="btn btn-default dropdown-toggle" type="button" ' +

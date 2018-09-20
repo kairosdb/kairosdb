8a49ebb2439b23c0ea582b97f9e42e5ce21b9786
Jeff Sabin <jsabin@proofpoint.com>
2018-09-20 11:03:24 -0600

    Removed the host tag from metric kairosdb.metric_counters because the tag cardinality can be really high with lots of Kairos hosts and lots of metrics. Fixed two broken tests and updated the pom file to include a new dependency.

--- src/test/java/org/kairosdb/core/datapoints/SnappyStringDataPointTest.java
+++ src/test/java/org/kairosdb/core/datapoints/SnappyStringDataPointTest.java
@@ -16,5 +16,7 @@ public class SnappyStringDataPointTest extends DataPointTestCommon
 		dataPointList.add(dpFactory.createDataPoint(123, "fo.com"));
 		dataPointList.add(dpFactory.createDataPoint(123, "123"));
 		dataPointList.add(dpFactory.createDataPoint(1234, "1.2.3.4 This is a long string that will get compressed by snappy :)"));
+
+		sum = 0;
 	}
 }
--- src/test/java/org/kairosdb/core/datapoints/StringDataPointTest.java
+++ src/test/java/org/kairosdb/core/datapoints/StringDataPointTest.java
@@ -16,5 +16,7 @@ public class StringDataPointTest extends DataPointTestCommon
 		dataPointList.add(dpFactory.createDataPoint(123, "fo.com"));
 		dataPointList.add(dpFactory.createDataPoint(123, "123"));
 		dataPointList.add(dpFactory.createDataPoint(1234, "1.2.3.4"));
+
+		sum = 0;
 	}
 }

dfbfe12c85ca1a4a9a2089ee846c2439c394eab5
Jeff Sabin <jsabin@proofpoint.com>
2018-10-10 16:39:52 -0600

    Modified RemoteDatastore to clean up zip files if the disk is filling up. Also added a health check to verify that the RemoteDatastore can send data to the remote host. Fixed bug where the kairosdb.datastore.remote.write_size metric was always returning 0.

--- src/main/java/org/kairosdb/datastore/remote/RemoteModule.java
+++ src/main/java/org/kairosdb/datastore/remote/RemoteModule.java
@@ -4,9 +4,8 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 import org.kairosdb.core.datastore.Datastore;
 import org.kairosdb.core.datastore.ServiceKeyStore;
-
-import javax.validation.constraints.Null;
-import javax.xml.ws.Service;
+import org.kairosdb.util.DiskUtils;
+import org.kairosdb.util.DiskUtilsImpl;
 
 /**
  Created with IntelliJ IDEA.
@@ -23,6 +22,9 @@ public class RemoteModule extends AbstractModule
 		bind(Datastore.class).to(RemoteDatastore.class).in(Scopes.SINGLETON);
 		bind(RemoteDatastore.class).in(Scopes.SINGLETON);
 		bind(RemoteSendJob.class).in(Scopes.SINGLETON);
+		bind(RemoteDatastoreHealthCheck.class).in(Scopes.SINGLETON);
 		bind(ServiceKeyStore.class).to(NullServiceKeyStore.class);
+		bind(DiskUtils.class).to(DiskUtilsImpl.class);
+		bind(RemoteHost.class).to(RemoteHostImpl.class);
 	}
 }

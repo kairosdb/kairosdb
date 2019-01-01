8333c3375c13d6e4797d6fa6787a644e0b293b6f
Brian Hawkins <brianhks1@gmail.com>
2019-01-01 00:01:59 -0700

    Fixed BatchHandler to remove failed inserts from cache #471

--- src/main/java/org/kairosdb/datastore/cassandra/CQLBatch.java
+++ src/main/java/org/kairosdb/datastore/cassandra/CQLBatch.java
@@ -15,9 +15,11 @@ import javax.inject.Named;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import static org.kairosdb.datastore.cassandra.CassandraDatastore.DATA_POINTS_ROW_KEY_SERIALIZER;
@@ -42,6 +44,10 @@ public class CQLBatch
 	private BatchStatement dataPointBatch = new BatchStatement(BatchStatement.Type.UNLOGGED);
 	private BatchStatement rowKeyBatch = new BatchStatement(BatchStatement.Type.UNLOGGED);
 
+	private List<DataPointsRowKey> m_newRowKeys = new ArrayList<>();
+	private List<String> m_newMetrics = new ArrayList<>();
+
+
 
 	@Inject
 	public CQLBatch(
@@ -59,6 +65,7 @@ public class CQLBatch
 
 	public void addRowKey(String metricName, DataPointsRowKey rowKey, int rowKeyTtl)
 	{
+		m_newRowKeys.add(rowKey);
 		ByteBuffer bb = ByteBuffer.allocate(8);
 		bb.putLong(0, rowKey.getTimestamp());
 
@@ -89,6 +96,7 @@ public class CQLBatch
 
 	public void addMetricName(String metricName)
 	{
+		m_newMetrics.add(metricName);
 		BoundStatement bs = new BoundStatement(m_clusterConnection.psStringIndexInsert);
 		bs.setBytesUnsafe(0, ByteBuffer.wrap(ROW_KEY_METRIC_NAMES.getBytes(UTF_8)));
 		bs.setString(1, metricName);
@@ -184,5 +192,15 @@ public class CQLBatch
 			m_clusterConnection.execute(dataPointBatch);
 			m_batchStats.addDatapointsBatch(dataPointBatch.size());
 		}
+	}
+
+	public List<DataPointsRowKey> getNewRowKeys()
+	{
+		return m_newRowKeys;
+	}
+
+	public List<String> getNewMetrics()
+	{
+		return m_newMetrics;
 	}
 }

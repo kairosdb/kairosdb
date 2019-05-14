06f6652f11fff17d6715d5693853f7d00e1949c5
Alex Korotkikh <alex.korotkykh@zalando.de>
2019-05-14 10:26:30 +0200

    add start and end timestamps to critical_query_finished log (#85)
    * add UUID for critical queries to correlate index and data_points queries result
    
    * propagate MaxRowKeysForQueryExceededException to higher level
    
    * add time span to critical_query_finished log
    
    * add start and end timestamps to critical_query_finished log
    
    * align naming with document

--- src/main/java/org/kairosdb/core/datastore/KairosDatastore.java
+++ src/main/java/org/kairosdb/core/datastore/KairosDatastore.java
@@ -441,9 +441,10 @@ public class KairosDatastore {
                 span.setTag("row_count", m_rowCount);
 
 				if (m_metric.getCriticalQueryUUID() != null) {
-					logger.info("critical_query_finished: uuid={} metric={} datapoint_count={} row_count={} time={}",
+					final long endTime = Long.MAX_VALUE == m_metric.getEndTime() ? System.currentTimeMillis() : m_metric.getEndTime();
+					logger.info("critical_query_finished: uuid={} metric={} datapoint_count={} row_count={} start_time={} end_time={} duration={}",
 							m_metric.getCriticalQueryUUID(), m_metric.getName(), m_dataPointCount, m_rowCount,
-							m_metric.getEndTime() - m_metric.getStartTime());
+							m_metric.getStartTime(), endTime, endTime - m_metric.getStartTime());
 				}
 
                 List<DataPointGroup> queryResults = groupByTypeAndTag(m_metric.getName(),

5140c47433bd5f1f49d175d85573aabbc150f423
Alex Korotkikh <me@alexkorotkikh.com>
2019-07-09 14:36:23 +0200

    introduce connection pooling (#96)
    * introduce connection pooling
    
    * increase pool limits
    
    * print connection pool info
    
    * change log level
    
    * add protocol version to logs

--- src/main/java/org/kairosdb/datastore/cassandra/CassandraClientImpl.java
+++ src/main/java/org/kairosdb/datastore/cassandra/CassandraClientImpl.java
@@ -1,61 +1,71 @@
 package org.kairosdb.datastore.cassandra;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.QueryOptions;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.SocketOptions;
+import com.datastax.driver.core.*;
 import com.datastax.driver.core.policies.DCAwareRoundRobinPolicy;
 import com.datastax.driver.core.policies.EC2AwareRoundRobinPolicy;
 import com.datastax.driver.core.policies.EC2MultiRegionAddressTranslator;
 import com.datastax.driver.core.policies.TokenAwarePolicy;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  Created by bhawkins on 3/4/15.
  */
-public class CassandraClientImpl implements CassandraClient
-{
+public class CassandraClientImpl implements CassandraClient {
+	private static final Logger logger = LoggerFactory.getLogger(CassandraClientImpl.class);
+
 	private final Cluster m_cluster;
 	private String m_keyspace;
 
-	public static final String CASSANDRA_READ_TIMEOUT = "kairosdb.datastore.cassandra.read.timeout";
+	private static final String CASSANDRA_READ_TIMEOUT = "kairosdb.datastore.cassandra.read.timeout";
 
 	@javax.inject.Inject
 	@Named(CASSANDRA_READ_TIMEOUT)
 	// We set the default timeout as configured by the upstream driver.
 	private int m_cassandraReadTimeout = 12000;
 
-
 	@Inject
-	public CassandraClientImpl(CassandraConfiguration config)
-	{
+	public CassandraClientImpl(CassandraConfiguration config) {
 		final Cluster.Builder builder = new Cluster.Builder().withSocketOptions(
 				new SocketOptions().setReadTimeoutMillis(m_cassandraReadTimeout));
 
-		if(config.getAddressTranslator().equals(CassandraConfiguration.ADDRESS_TRANSLATOR_TYPE.EC2)) {
+		if (config.getAddressTranslator().equals(CassandraConfiguration.ADDRESS_TRANSLATOR_TYPE.EC2)) {
 			builder.withAddressTranslator(new EC2MultiRegionAddressTranslator());
 			// This should work, seems the EC2AwareRoundRobinPolicy uses REMOTE for not being in the SAME az
 			builder.withLoadBalancingPolicy(new TokenAwarePolicy(EC2AwareRoundRobinPolicy.CreateEC2AwareRoundRobinPolicy()));
-		}
-		else {
+		} else {
 			builder.withLoadBalancingPolicy(new TokenAwarePolicy(DCAwareRoundRobinPolicy.builder().build()));
 		}
 
-		builder.withQueryOptions(new QueryOptions().setConsistencyLevel(config.getDataReadLevel()));
+		final QueryOptions queryOptions = new QueryOptions().setConsistencyLevel(config.getDataReadLevel());
+		builder.withQueryOptions(queryOptions);
 
 		for (String node : config.getHostList().split(",")) {
 			builder.addContactPoint(node.trim());
 		}
 
-		String user = config.getUser();
-		String password = config.getPassword();
-		if(null!=user && null!=password && !"".equals(user) && !"".equals(password)) {
+		final String user = config.getUser();
+		final String password = config.getPassword();
+		if (null != user && null != password && !"".equals(user) && !"".equals(password)) {
 			builder.withCredentials(user, password);
 		}
 
 		m_cluster = builder.build();
 		m_keyspace = config.getKeyspaceName();
+
+		final PoolingOptions poolOpts = m_cluster.getConfiguration().getPoolingOptions();
+		logger.info("Core connections per host (remote): " + poolOpts.getCoreConnectionsPerHost(HostDistance.REMOTE));
+		logger.info("Core connections per host (local): " + poolOpts.getCoreConnectionsPerHost(HostDistance.LOCAL));
+		logger.info("Max connections per host (remote): " + poolOpts.getMaxConnectionsPerHost(HostDistance.REMOTE));
+		logger.info("Max connections per host (local): " + poolOpts.getMaxConnectionsPerHost(HostDistance.LOCAL));
+		logger.info("Max requests per connection (remote): " + poolOpts.getMaxRequestsPerConnection(HostDistance.REMOTE));
+		logger.info("Max requests per connection (local): " + poolOpts.getMaxRequestsPerConnection(HostDistance.LOCAL));
+		logger.info("Max queue size: " + poolOpts.getMaxQueueSize());
+		logger.info("Pool timeout mills: " + poolOpts.getPoolTimeoutMillis());
+		logger.info("Idle timeout seconds: " + poolOpts.getIdleTimeoutSeconds());
+		logger.info("Protocol options: " + m_cluster.getConfiguration().getProtocolOptions().getProtocolVersion().toString());
 	}
 
 

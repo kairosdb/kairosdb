0506aeae93ff8ca7fe8d006a3b34c5930213e099
Alex Korotkykh <me@alexkorotkikh.com>
2019-11-21 20:09:49 +0100

    use an abs of the modulo

--- src/main/java/org/kairosdb/datastore/cassandra/cache/CacheWarmingUpLogic.java
+++ src/main/java/org/kairosdb/datastore/cassandra/cache/CacheWarmingUpLogic.java
@@ -15,12 +15,13 @@ public class CacheWarmingUpLogic {
         }
         final int numberOfRows = minutesBeforeNextBucket / rowSize;
         final long currentRowOfGracePeriod = (currentTime - warmingUpPeriodStartsAt) / 1000 / 60 / rowSize;
-        boolean result = hashCode % numberOfRows == currentRowOfGracePeriod;
+        final int modulo = Math.abs(hashCode % numberOfRows);
+        boolean result = modulo == currentRowOfGracePeriod;
         logger.debug(String.format("Result '%b' is calculated based on following: " +
                         "hash code of byte buffer = '%d', " +
                         "number of rows = '%d', current row of grace period = '%d', " +
                         "hashCode %% numberOfRows is '%d'",
-                result, hashCode, numberOfRows, currentRowOfGracePeriod, hashCode % numberOfRows));
+                result, hashCode, numberOfRows, currentRowOfGracePeriod, modulo));
         return result;
     }
 }

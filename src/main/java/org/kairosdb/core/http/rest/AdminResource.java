98300182b5e6df31dbb196b20d60ea720f5ffb32
Brian Hawkins <brianhks1@gmail.com>
2020-02-13 12:14:26 -0700

    Reformat pull request code

--- src/main/java/org/kairosdb/core/http/rest/AdminResource.java
+++ src/main/java/org/kairosdb/core/http/rest/AdminResource.java
@@ -1,17 +1,15 @@
 package org.kairosdb.core.http.rest;
 
-import com.google.common.collect.SetMultimap;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.kairosdb.core.datastore.QueryMetric;
 import org.kairosdb.core.datastore.QueryQueuingManager;
 import org.kairosdb.core.http.rest.json.ErrorResponse;
-import org.kairosdb.plugin.Aggregator;
-import org.kairosdb.plugin.GroupBy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.agileclick.genorm.runtime.Pair;
+
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -25,70 +23,71 @@ import static org.kairosdb.core.http.rest.MetricsResource.setHeaders;
 @Path("api/v1/admin")
 public class AdminResource
 {
-    private static final Logger logger = LoggerFactory.getLogger(AdminResource.class);
+	private static final Logger logger = LoggerFactory.getLogger(AdminResource.class);
 
-    private final QueryQueuingManager m_queuingManager;
+	private final QueryQueuingManager m_queuingManager;
 
-    @Inject
-    public AdminResource(QueryQueuingManager queuingManager)
-    {
-        this.m_queuingManager = checkNotNull(queuingManager, "queuingManager cannot be null.");
-    }
+	@Inject
+	public AdminResource(QueryQueuingManager queuingManager)
+	{
+		this.m_queuingManager = checkNotNull(queuingManager, "queuingManager cannot be null.");
+	}
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
-    @Path("/runningqueries")
-    public Response listRunningQueries()
-    {
-        try
-        {
-            int queriesWaitingCount = m_queuingManager.getQueryWaitingCount();
-            ArrayList<Pair<String, QueryMetric>> runningQueries = m_queuingManager.getRunningQueries();
-            JsonArray queryInfo = new JsonArray();
+	@GET
+	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+	@Path("/runningqueries")
+	public Response listRunningQueries()
+	{
+		try
+		{
+			int queriesWaitingCount = m_queuingManager.getQueryWaitingCount();
+			ArrayList<Pair<String, QueryMetric>> runningQueries = m_queuingManager.getRunningQueries();
+			JsonArray queryInfo = new JsonArray();
 
-            JsonObject responseJson = new JsonObject();
-            for (Pair<String, QueryMetric> query : runningQueries) {
-                JsonObject queryJson = new JsonObject();
-                String queryHash = query.getFirst();
-                QueryMetric queryMetric = query.getSecond();
-                queryJson.addProperty("query hash", queryHash);
-                queryJson.addProperty("metric name", queryMetric.getName());
-                queryJson.add("query JSON", queryMetric.getJsonObj());
+			JsonObject responseJson = new JsonObject();
+			for (Pair<String, QueryMetric> query : runningQueries)
+			{
+				JsonObject queryJson = new JsonObject();
+				String queryHash = query.getFirst();
+				QueryMetric queryMetric = query.getSecond();
+				queryJson.addProperty("query hash", queryHash);
+				queryJson.addProperty("metric name", queryMetric.getName());
+				queryJson.add("query JSON", queryMetric.getJsonObj());
 
-                queryInfo.add(queryJson);
-            }
-            responseJson.add("queries", queryInfo);
-            responseJson.addProperty("queries waiting", queriesWaitingCount);
+				queryInfo.add(queryJson);
+			}
+			responseJson.add("queries", queryInfo);
+			responseJson.addProperty("queries waiting", queriesWaitingCount);
 
-            logger.debug("Listing running queries.");
-            Response.ResponseBuilder responseBuilder = Response.status(Response.Status.OK).entity(responseJson.toString());
-            setHeaders(responseBuilder);
-            return responseBuilder.build();
+			logger.debug("Listing running queries.");
+			Response.ResponseBuilder responseBuilder = Response.status(Response.Status.OK).entity(responseJson.toString());
+			setHeaders(responseBuilder);
+			return responseBuilder.build();
 
-        }
-        catch (Exception e)
-        {
-            logger.error("Failed to get running queries.", e);
-            return setHeaders(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ErrorResponse(e.getMessage()))).build();
-        }
-    }
+		}
+		catch (Exception e)
+		{
+			logger.error("Failed to get running queries.", e);
+			return setHeaders(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ErrorResponse(e.getMessage()))).build();
+		}
+	}
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
-    @Path("/killquery/{queryHash}")
-    public Response killQueryByHash(@PathParam("queryHash") String queryHash)
-    {
-        try
-        {
-            m_queuingManager.killQuery(queryHash);
-            logger.info("Killed query by hash: " + queryHash);
-            return setHeaders(Response.status(Response.Status.NO_CONTENT)).build();
-        }
-        catch (Exception e)
-        {
-            logger.error("Failed to kill query by hash: " + queryHash, e);
-            return setHeaders(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ErrorResponse(e.getMessage()))).build();
-        }
-    }
+	@GET
+	@Produces(MediaType.APPLICATION_JSON + "; charset=UTF-8")
+	@Path("/killquery/{queryHash}")
+	public Response killQueryByHash(@PathParam("queryHash") String queryHash)
+	{
+		try
+		{
+			m_queuingManager.killQuery(queryHash);
+			logger.info("Killed query by hash: " + queryHash);
+			return setHeaders(Response.status(Response.Status.NO_CONTENT)).build();
+		}
+		catch (Exception e)
+		{
+			logger.error("Failed to kill query by hash: " + queryHash, e);
+			return setHeaders(Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ErrorResponse(e.getMessage()))).build();
+		}
+	}
 
 }

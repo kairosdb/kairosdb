2346840bd5d1924408d9a520738969b5b6b5b912
Brian Hawkins <brianhks1@gmail.com>
2021-09-20 15:43:11 -0600

    Updated query script

--- src/scripts/kairos_query.rb
+++ src/scripts/kairos_query.rb
@@ -17,9 +17,7 @@ require 'uri'
 require 'net/http'
 require 'date'
 
-$query = {}
-$query[:metrics] = []
-$query[:metrics][0] = {}
+$query = { metrics: [{}]}
 
 $host = 'http://localhost:8080'
 
@@ -56,9 +54,7 @@ class Monitor
 
 	def add_warn_message(msg)
 		@warn_messages.append(msg)
-		if @return_code == 0
-			@return_code = 1
-		end
+		@return_code = 1 if @return_code == 0
 	end
 
 	def get_message
@@ -74,14 +70,38 @@ class Monitor
 
 end
 
+class QueryResults
+
+	def initialize(query_results)
+		@query_results = query_results
+	end
+
+	def empty?
+		@query_results.nil?
+	end
+
+	def each
+		@query_results.each do |result|
+
+			name = result['name']
+
+			groups = {}
+
+			if result['group_by']
+				result['group_by'].each do |group_by|
+					groups = group_by['group'] if group_by['name'] == 'tag'
+				end
+			end
+
+			yield(name, groups, result['values'])
+		end
+	end
+end
+
 $monitor = Monitor.new
 
 def parse_time(param)
-	_, number, unit = /(\d+)(\w+)/.match(param).to_a.flatten
-
-	# if result is nill then fail with error
-	# p number
-	# p unit
+	_, number, unit = /(\d+)(\w+)/.match(param).to_a
 
 	case unit
 	when 'ms'
@@ -106,60 +126,109 @@ end
 def get_sampling(sampling)
 	number, unit = parse_time(sampling)
 
-	ret = {}
-	ret[:value] = number
-	ret[:unit] = unit
-
-	ret
+	{value: value, unit: unit}
 end
 
-def print_results(name, groups, values)
-	group_str_arr = []
+def print_results(query_results)
+	unless query_results.nil?
 
-	groups.each do |key, value|
-		group_str_arr.append("#{key}=#{value}")
-	end
+		query_results.each do |name, groups, values|
+			group_str_arr = []
 
-	values.each do |value|
-		str = "#{Time.at(value[0] / 1000).to_datetime.strftime('%FT%T.%L%:z')} "
-		str += "#{group_str_arr.join(',')} #{name}=#{value[1].to_s}"
-		puts str
+			groups.each do |key, value|
+				group_str_arr.append("#{key}=#{value}")
+			end
+
+			values.each do |value|
+				str = "#{Time.at(value[0] / 1000).to_datetime.strftime('%FT%T.%L%:z')} "
+				str += "#{group_str_arr.join(',')} #{name}=#{value[1].to_s}"
+				puts str
+			end
+		end
 	end
 end
 
-def check_results(_name, groups, values)
-	group_str_arr = []
+def check_results(query_results)
 
-	groups.each do |key, value|
-		group_str_arr.append("#{key}=#{value}")
-	end
+	if !query_results.nil?
+		query_results.each do |name, groups, values|
+			group_str_arr = []
 
-	values.each do |value|
-		if !$monitor.crit_min.nil? && value[1].to_f < $monitor.crit_min
-			$monitor.add_crit_message("[#{group_str_arr.join(',')}] #{value[1]} is less than mincrit of #{$monitor.crit_min}")
-			break
+			groups.each do |key, value|
+				group_str_arr.append("#{key}=#{value}")
+			end
+
+			if values.empty?
+				case $monitor.on_empty
+				when :warn
+					$monitor.add_warn_message('Empty results returned from query.')
+				when :crit
+					$monitor.add_crit_message('Empty results returned from query.')
+				end
+			else
+				warn_message = nil
+				values.each do |value|
+					if !$monitor.crit_min.nil? && value[1].to_f < $monitor.crit_min
+						$monitor.add_crit_message("[#{group_str_arr.join(',')}] #{value[1]} is less than mincrit of #{$monitor.crit_min}")
+						break
+					end
+
+					if !$monitor.crit_max.nil? &&  value[1].to_f > $monitor.crit_max
+						$monitor.add_crit_message("[#{group_str_arr.join(',')}] #{value[1]} is greater than maxcrit of #{$monitor.crit_max}")
+						break
+					end
+
+					# If we get a critical then we bail out.  We don't bail out on warnings
+					# in case a critical is yet to come.
+
+					if !$monitor.warn_min.nil? && value[1].to_f < $monitor.warn_min
+						warn_message = "[#{group_str_arr.join(',')}] #{value[1]} is less than minwarn of #{$monitor.warn_min}"
+					end
+
+					if !$monitor.warn_max.nil? && value[1].to_f > $monitor.warn_max
+						warn_message = "[#{group_str_arr.join(',')}] #{value[1]} is greater than maxwarn of #{$monitor.warn_max}"
+					end
+
+				end
+
+				$monitor.add_warn_message(warn_message) unless warn_message.nil?
+			end
 		end
-
-		if !$monitor.crit_max.nil? &&  value[1].to_f > $monitor.crit_max
-			$monitor.add_crit_message("[#{group_str_arr.join(',')}] #{value[1]} is greater than maxcrit of #{$monitor.crit_max}")
-			break
+	else
+		case $monitor.on_empty
+		when :warn
+			$monitor.add_warn_message('Empty results returned from query.')
+		when :crit
+			$monitor.add_crit_message('Empty results returned from query.')
 		end
-
-		if !$monitor.warn_min.nil? && value[1].to_f < $monitor.warn_min
-			$monitor.add_warn_message("[#{group_str_arr.join(',')}] #{value[1]} is less than minwarn of #{$monitor.warn_min}")
-			break
-		end
-
-		if !$monitor.warn_max.nil? && value[1].to_f > $monitor.warn_max
-			$monitor.add_warn_message("[#{group_str_arr.join(',')}] #{value[1]} is greater than maxwarn of #{$monitor.warn_max}")
-			break
-		end
-
 	end
 end
 
 OptionParser.new do |opts|
-	opts.banner = 'Usage: bla bla bla'
+	opts.banner =
+		'KairosDB query/monitor script.  This script lets you both query Kairos for data
+as well as monitor data.  The monitoring was designed to be used with Nagios NRPE checks.
+Usage: kairos_query.rb -h <kairos_host> -m <metric> -s <start_time> -e <end_time>
+    -t <filter_tags> -a <aggregators> -g <groups>
+
+For time options such as start and end time the format is a number followed by a time unit,
+ex. 10m (10 minutes) or 30s (30 seconds).
+Allowed time units are ms, s, m, h, d, w, y.
+
+Supported aggregators are sum, avg, min, max, count and trim.  For sum, avg,
+min, max and count you have to specify a sampling period, for example sum(10m) - this
+will sum over a 10 minute window.
+
+If you specify one or more of the monitor options (minwarn, maxwarn, mincrit or maxcrit)
+the script runs in monitor mode.  Monitor mode does not return data but returns codes
+according to monitor options specified.
+0 - if all values are withing specified range.
+1 - if any value is in the warn range.
+2 - if any value is in the critical range.
+
+Monitor mode was designed to be used as an NRPE check with nagios.
+
+Available options:'
 
 	opts.on('-h', '--host <kairos url>', 'Ulr to Kairos host') do |v|
 		$host = v
@@ -177,20 +246,20 @@ OptionParser.new do |opts|
 		start[:unit] = unit
 	end
 
-	opts.on('-e', '--end <end time>', 'Start of query') do |v|
+	opts.on('-e', '--end <end time>', 'End of query') do |v|
 		end_time = $query[:end_relative] = {}
 		number, unit = parse_time v
 		end_time[:value] = number
 		end_time[:unit] = unit
 	end
 
-	opts.on('-t', '--tags <key=value,key2=value,...>', 'Start of query') do |v|
+	opts.on('-t', '--tags <key=value,key2=value,...>', 'Tags to filter on') do |v|
 		tags = $metric_query[:tags] = {}
 
 		tag_arr = v.split(',')
 
 		tag_arr.each do |tag_pair|
-			_, key, value = /(\w+)=(\w+)/.match(tag_pair).to_a.flatten
+			_, key, value = /(.+)=(.+)/.match(tag_pair).to_a
 
 			tags[key] = [] if tags[key].nil?
 
@@ -198,13 +267,13 @@ OptionParser.new do |opts|
 		end
 	end
 
-	opts.on('-a', '--aggregate <aggregator string>', 'Start of query') do |v|
+	opts.on('-a', '--aggregate <aggregator string>', 'Aggregators for data.  Multiple aggregators are piped together "sum(1m)|avg(10m)"') do |v|
 		aggregators = v.split('|')
 		# puts aggregators
 		query_aggs = $metric_query[:aggregators] = []
 
 		aggregators.each do |agg|
-			_, agg_name, argument = /(\w+)\((.*)\)/.match(agg).to_a.flatten
+			_, agg_name, argument = /(\w+)\((.*)\)/.match(agg).to_a
 
 			agg_json = {}
 			agg_json[:name] = agg_name
@@ -222,7 +291,7 @@ OptionParser.new do |opts|
 		end
 	end
 
-	opts.on('-g', '--group-by <group1,group2,...>', 'Start of query') do |v|
+	opts.on('-g', '--group-by <group1,group2,...>', 'Tags to group on') do |v|
 		groups = v.split(',')
 		$metric_query[:group_by] = []
 		group_tags = {}
@@ -260,7 +329,7 @@ OptionParser.new do |opts|
 		end
 	end
 
-	opts.on('--on_empty ok|warn|crit',
+	opts.on('--on-empty ok|warn|crit',
 					'If no metrics are returned should it be "ok" "warn" or "crit" (default warn)') do |v|
 		case v
 		when 'ok'
@@ -274,15 +343,15 @@ OptionParser.new do |opts|
 		end
 	end
 
-	opts.on('--on_timeout <value>', 'Maximum critical threshold to monitor') do |v|
-		$monitor.crit_max = v
+	opts.on('--on-timeout <value>', 'Maximum critical threshold to monitor') do |v|
+		$monitor.on_timeout = v
 	end
 end.parse!
 
-# puts query.to_json
+#puts $query.to_json
 uri = URI.parse("#{$host}/api/v1/datapoints/query")
 http = Net::HTTP.new(uri.host, uri.port)
-request = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+request = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
 request.body = $query.to_json
 
 response = http.request(request)
@@ -295,30 +364,16 @@ json_resp = JSON.parse(response.body)
 
 
 # We only query a single metric so there is only one query result
-query_results = json_resp['queries'][0]['results']
+query_results = QueryResults.new(json_resp['queries'][0]['results'])
 
-# puts query_results[0]
-unless query_results.nil?
-	query_results.each do |result|
-
-		name = result['name']
-		groups = {}
-
-		result['group_by'].each do |group_by|
-			groups = group_by['group'] if group_by['name'] == 'tag'
-		end
-
-		if $monitor.monitor_set?
-			check_results(name, groups, result['values'])
-		else
-			print_results(name, groups, result['values'])
-		end
-	end
-end
-
-
+#puts json_resp['queries'][0]['results']
 
 if $monitor.monitor_set?
+	check_results(query_results)
 	puts $monitor.get_message
 	exit($monitor.return_code)
+else
+	print_results(query_results)
 end
+
+

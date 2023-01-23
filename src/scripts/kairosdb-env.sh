b462c265b54befa2dc80f5c1dcd3ebdd4ff1413d
Brian Hawkins <bhawkins@C02FL5CJMD6R.corp.proofpoint.com>
2023-01-22 21:04:38 -0700

    fixed env relative path

--- src/scripts/kairosdb-env.sh
+++ src/scripts/kairosdb-env.sh
@@ -1,5 +1,5 @@
 
 #JAVA_OPTS="$JAVA_OPTS -Xmx4G"  # Max heap size
 #JAVA_OPTS="$JAVA_OPTS -Xms4G"  # Min heap size
-JAVA_OPTS="$JAVA_OPTS -DMETRICS4J_CONFIG=/opt/kairosdb/conf/metrics/metrics4j.conf"
+JAVA_OPTS="$JAVA_OPTS -DMETRICS4J_CONFIG=./conf/metrics/metrics4j.conf"
 
